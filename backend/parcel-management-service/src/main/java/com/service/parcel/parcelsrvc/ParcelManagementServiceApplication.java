c82aa48de6ca8b0f4160f6e4da7d77f3404027a8
jkhare60 <jkhare60@gmail.com>
2025-01-14 01:02:59 +0530

    Initial commit

---
+++ backend/parcel-management-service/src/main/java/com/service/parcel/parcelsrvc/ParcelManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.parcel.parcelsrvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcelManagementServiceApplication.class, args);
+	}
+
+}
---
+++ backend/parcel-management-service/src/test/java/com/service/parcel/parcelsrvc/ParcelManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.parcel.parcelsrvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
